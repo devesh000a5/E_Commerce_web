97878e42a5f24a099f31aa7c5ddbbd120c146504
Devesh Singh <deveshsingh23503@gmail.com>
2025-05-27 22:59:57 +0530

    changes

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -18,7 +18,7 @@ const Signup = () => {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/signup", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password, name }),
